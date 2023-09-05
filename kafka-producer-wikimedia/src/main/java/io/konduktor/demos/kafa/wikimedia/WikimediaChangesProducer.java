56da73ac217d6d40235bf0151236abc78a2a7e1c
pcnnh <tunguyencntt1996@gmail.com>
2023-09-05 17:44:55 +0700

    Change to use elasticsearch instead of opensearch

--- kafka-producer-wikimedia/src/main/java/io/konduktor/demos/kafa/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/konduktor/demos/kafa/wikimedia/WikimediaChangesProducer.java
@@ -29,6 +29,16 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
+        // set safe producer configs (Kafka <= 2.8)
+//        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+//        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+//        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        // set producer compression config for higher throughput (in expense for latency)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); // 20ms
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32KB
+
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         String eventUrl = "https://stream.wikimedia.org/v2/stream/recentchange";
